fa8c3bbeee646f57d2b73837721b68f72b150ac6
jasperslowik <jasper.slowik@icloud.com>
2025-04-04 16:58:10 +0200

    Update uvicorn.run call to use "main:app" for improved clarity in application entry point

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     
     
         
