9fe1c2195774dc8033c75a8e82465fe68db78567
jasperslowik <jasper.slowik@icloud.com>
2025-04-24 18:39:00 +0200

    Fix Heroku deployment by disabling reload mode

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def read_root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 
     
