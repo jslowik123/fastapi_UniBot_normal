5878e5878efdab277f15ab83328cb09d0d34f9e1
jasperslowik <jasper.slowik@icloud.com>
2025-05-10 17:35:58 +0200

    qa

--- celery_app.py
+++ celery_app.py
@@ -6,7 +6,7 @@ import os
 redis_url = "rediss://:p277c32da1640e7109e3e7fd52cd4c73d5b85ac54e4972db72473a2fa0b02742d@ec2-54-171-238-172.eu-west-1.compute.amazonaws.com:31030?ssl_cert_reqs=CERT_NONE"
 
 celery = Celery("myapp", broker=redis_url, backend=redis_url)
-app.conf.update(
+celery.conf.update(
     broker_url=redis_url,
     result_backend=redis_url,
     broker_use_ssl={
