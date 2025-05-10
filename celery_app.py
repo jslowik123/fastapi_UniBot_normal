67517cf8ea81f4a77604235428156a8bffa5a147
jasperslowik <jasper.slowik@icloud.com>
2025-05-10 08:23:43 +0200

    redis

--- celery_app.py
+++ celery_app.py
@@ -1,9 +1,12 @@
 from celery import Celery
 import os
 
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+
+redis_url = "rediss://:p277c32da1640e7109e3e7fd52cd4c73d5b85ac54e4972db72473a2fa0b02742d@ec2-54-171-238-172.eu-west-1.compute.amazonaws.com:31030"
 
 celery = Celery("myapp", broker=redis_url, backend=redis_url)
+
 
 @celery.task
 def test_task():
