d335cf270bd836c65ef7867de0bf92445eba3fb7
jasperslowik <jasper.slowik@icloud.com>
2025-05-10 19:29:32 +0200

    mal schauen

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-8000}
-worker: celery -A celery_app.celery worker --loglevel=info
+web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-8000} --timeout-keep-alive 75 --timeout-graceful-shutdown 30
+worker: celery -A celery_app.celery worker --loglevel=info --pool=solo
--- celery_app.py
+++ celery_app.py
@@ -6,7 +6,11 @@ import ssl
 
 redis_url = "rediss://:p277c32da1640e7109e3e7fd52cd4c73d5b85ac54e4972db72473a2fa0b02742d@ec2-54-171-238-172.eu-west-1.compute.amazonaws.com:31030"
 
-celery = Celery("myapp", broker=redis_url, backend=redis_url)
+celery = Celery("myapp",
+                broker=redis_url,
+                backend=redis_url,
+                include=['tasks'])
+
 celery.conf.update(
     broker_url=redis_url,
     result_backend=redis_url,
@@ -24,7 +28,11 @@ celery.conf.update(
         'ssl_keyfile': None,
         'ssl_check_hostname': False
     },
-    broker_connection_retry_on_startup=True
+    broker_connection_retry_on_startup=True,
+    task_track_started=True,
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json']
 )
 
 @celery.task
