309d0edf9bf7fe0ff14f42f5fcd7206c3a81a34a
jasperslowik <jasper.slowik@icloud.com>
2025-05-10 08:32:05 +0200

    a

--- celery_app.py
+++ celery_app.py
@@ -3,7 +3,7 @@ import os
 
 
 
-redis_url = "rediss://:p277c32da1640e7109e3e7fd52cd4c73d5b85ac54e4972db72473a2fa0b02742d@ec2-54-171-238-172.eu-west-1.compute.amazonaws.com:31030"
+redis_url = "rediss://:p277c32da1640e7109e3e7fd52cd4c73d5b85ac54e4972db72473a2fa0b02742d@ec2-54-171-238-172.eu-west-1.compute.amazonaws.com:31030?ssl_cert_reqs=CERT_NONE"
 
 celery = Celery("myapp", broker=redis_url, backend=redis_url)
 celery.conf.update(
