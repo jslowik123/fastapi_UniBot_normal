d7b39428aad6b83d2fc04b042ae312d6449e0e6b
Jasper Slowik <jasperslowik@gmail.com>
2025-06-04 17:14:30 +0200

    dies ist ein test

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from doc_processor import DocProcessor
 import os
+import requests
+
 if __name__ == "__main__":
-    proc = DocProcessor( os.getenv("PINECONE_API_KEY"), os.getenv("OPENAI_API_KEY"),)
-    proc.generate_global_summary("was laeuft")
+    bot = requests.post("https://uni-chatbot-e2bc39ffc8de.herokuapp.com/start_bot")
+    response = requests.post("https://uni-chatbot-e2bc39ffc8de.herokuapp.com/send_message", data={"user_input": "Was ist Python?", "namespace": "zwischenpraesentation"})
+    print(f"Status: {response.status_code}, Response: {response.json()}")
+    
